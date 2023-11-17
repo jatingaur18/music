d9082c7e64df177a26e3273908ef19c08519cb39
jatin gaur <jatin22101@iiitnr.edu.in>
2023-11-17 19:27:15 +0530

    storing file

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const { GridFsStorage } = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
 
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
@@ -12,6 +19,9 @@ const indexRouter = require('./routes/index')
 const authRouter = require('./routes/logandsign')
 const homeRouter = require('./routes/home')
 
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -21,15 +31,51 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 const mongoose = require('mongoose');
 const newuser = require('./models/newuser')
-mongoose.connect(process.env.DATABASE_URL ,{useNewUrlParser: true})
-
+mongoose.connect(process.env.DATABASE_URL ,{useNewUrlParser: true});
 
 const db = mongoose.connection
 db.on('error',error => console.error(error))
-db.once('open',()=> console.log('Connected to Mongoose'))
+//db.once('open',()=> console.log('Connected to Mongoose'))
+
+let gfs;
+
+db.once('open',()=>{
+    console.log('Connected to Mongoose'),
+    gfs = Grid(db.db,mongoose.mongo);
+    gfs.collection('songs');
+})
+
+ 
+
 app.use('/',indexRouter)
 app.use('/',authRouter)
 app.use('/',homeRouter)
+
+
+let filen;
+
+const storage = new GridFsStorage({
+    
+    url: 'mongodb+srv://MicroftHolmes:rvq6vjPrtPQSyGk7@cluster0.gq5rvso.mongodb.net/test',
+    file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'songs'
+        };
+        resolve(fileInfo);
+        filen=fileInfo.filename;
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
 
 const currUser={
     "name":"login",
@@ -80,4 +126,85 @@ app.post('/login',async (req,res)=>{
 
 })
 
-app.listen(process.env.PORT ||3000)
+app.post('/upload',upload.single('file'),(req,res)=>{
+    res.json({file: req.file});
+    console.log(filen);
+});
+
+
+app.get('/files', async (req, res) => {
+    try {
+        const files = await gfs.files.find().toArray();
+        if (!files || files.length === 0) {
+            return res.status(404).json({ err: 'No files exist' });
+        }
+        return res.json(files);
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/files/:filename', async (req, res) => {
+    try {
+        const file = await gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(file);
+                }
+            
+        });
+
+        // Check if file
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        }
+
+        // File exists
+        return res.json(file);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            err: 'Internal Server Error'
+        });
+    }
+});
+
+app.get('/image/:filename', async (req, res) => {
+    
+        const file = await gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(file);
+                }
+            
+        });
+
+        // Check if file
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        }
+
+       // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read output to browser
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({
+          err: 'Not an image'
+        });
+      }
+        
+    
+    
+});
+
+
+
+app.listen(process.env.PORT ||3001)
